3e54485043eb5e18610a40e8f1c58d0303133bcf
PolopTech <73751987+PolopTechnology@users.noreply.github.com>
2021-03-04 18:42:23 -0500

    FINALLY!

--- Java/Quick Union/App.java
+++ Java/Quick Union/App.java
@@ -1,39 +1,15 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Integer[][] N = {{1},{2},{3}};
-        int[] times = {1, 2, 3};
-        int[] sectimes = {3, 2, 2};
-        int no = times.length;
-        int yes = sectimes.length;
-        if(no == yes){
-            ConnectAll(no, N, times, sectimes);
-        }
+        int[] id = {1, 2, 3, 4, 5};
+        Union(id, 5, 1);
+        Union(id, 2, 5);
+        Union(id, 3, 1);
+        Union(id, 4, 3);
     }
-
-    public static Integer[] append(Integer[] arr, int element) {
-        List<Integer> list = new ArrayList<>(Arrays.asList(arr));
-        list.add(element);
- 
-        return list.toArray(new Integer[0]);
-    }
-    public static void ConnectAll(int times, Integer[][] id, int[] firstArrays, int[] secArrays){
-        for(int i = 0; i < times; i++){
-            Union(id, firstArrays[i], secArrays[i]);
-        }
-    }
-    public static void Union(Integer[][] id, int p, int q){
-        for(Integer[] x: id){
-            for(int y: x){
-                if(y == q){
-                     Integer[] news = append(x, p);
-                     System.out.println(Arrays.toString(news));
-                }
-            }
-// 0
-        }
+    public static void Union(int[] id, int p, int q){
+        id[p -1] = id[q -1];
+        System.out.println(Arrays.toString(id));
     }
 }
