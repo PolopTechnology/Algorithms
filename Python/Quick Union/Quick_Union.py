aae6fdf9fa5b15bd60497e24cf33a714183885f2
PolopTech <73751987+PolopTechnology@users.noreply.github.com>
2021-03-04 13:52:24 -0500

    Quick Union Py Improved

--- Python/Quick Union/Quick_Union.py
+++ Python/Quick Union/Quick_Union.py
@@ -4,9 +4,13 @@ def Connect(num1, num2):
     for x in array:
         if num2 in x:
             x.append(num1)
-            break
+            for y in array:
+                if num1 in y:
+                    y.remove(num1)
+                    break
     print(array)
 
-Connect(1, 5)
-Connect(2, 5)
-Connect(4, 3)
+while True:
+    q = int(input())
+    p = int(input())
+    Connect(q, p)
